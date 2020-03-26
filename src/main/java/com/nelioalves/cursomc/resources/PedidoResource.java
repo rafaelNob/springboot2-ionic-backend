c8fa2f9c71f489689ba5f3b2d4b69428e2615423
Rafael Noberto de França <Rafael.Franca@dellavolpe.com.br>
2020-03-26 14:34:46 -0300

    Implementando perfil de teste

--- src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.nelioalves.cursomc.domain.Cep;
 import com.nelioalves.cursomc.domain.Pedido;
 import com.nelioalves.cursomc.services.PedidoService;
 
@@ -25,6 +30,7 @@ public class PedidoResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+		PedidoResource.consumindoCep();
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -35,5 +41,23 @@ public class PedidoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	/**
+	 * METODO PARA CONSUMIR apis externos
+	 */
+	private static void consumindoCep() {
+		//https://viacep.com.br/ws/01001000/json/
+		RestTemplate rt = new RestTemplate();
+		 UriComponents uri = UriComponentsBuilder.newInstance()
+				.scheme("https")
+				.host("viacep.com.br")
+				.path("ws/01001000/json/")
+				.build();	
+		 
+		 ResponseEntity<Cep> forEntity = rt.getForEntity(uri.toUriString(), Cep.class);
+		 
+		 System.out.println(forEntity.getBody().getLogradouro());
+		
+		
 	}
 }
