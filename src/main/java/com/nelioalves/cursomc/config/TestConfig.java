98862b2187d2ad8bf4163794e93c21ca81c18aa9
Rafael Noberto de França <Rafael.Franca@dellavolpe.com.br>
2020-03-27 17:39:11 -0300

    implementação envio de email Gmail

--- src/main/java/com/nelioalves/cursomc/config/TestConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/TestConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
 import com.nelioalves.cursomc.services.EmailService;
-import com.nelioalves.cursomc.services.MockEmailService;
+import com.nelioalves.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("test")
@@ -30,6 +30,6 @@ public class TestConfig {
 	public EmailService emailService() {
 		
 		
-		return new MockEmailService();
+		return new SmtpEmailService();
 	}
 }
